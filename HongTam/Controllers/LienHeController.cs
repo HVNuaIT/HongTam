8330d09ca7f0747b4f51346b42c1a114d313a244
HV Nữa <56875666+HVNuaIT@users.noreply.github.com>
2022-12-06 20:46:21 +0700

    Cập Nhật Layout

---
+++ HongTam/Controllers/LienHeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HongTam.Controllers
+{
+    public class LienHeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
